01a3707542169386c3f1f9de2c675687110fb169
aswanigopinathmg-tech <aswanigopinathmg@gmail.com>
2025-09-26 15:58:06 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Adjusted config for root-level main.jsx
 export default defineConfig({
   plugins: [react()],
-  base: '/sku_app/',   // your repo name
-  root: '.',           // use current folder as root
-  build: {
-    rollupOptions: {
-      // tell rollup/vite that index.html at root is the entry
-      input: './index.html'
-    }
-  }
+  base: '/sku_app/',
 })
