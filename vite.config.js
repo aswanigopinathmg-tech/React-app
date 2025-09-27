a4898d55fb258c8d40b5f32895e55e7560c71c0c
aswanigopinathmg-tech <aswanigopinathmg@gmail.com>
2025-09-27 12:40:19 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   
+  root: '.', // explicitly tell Vite root is current folder
+  base: '/', // your base path
+  build: {
+    outDir: 'dist', // built files go here
+  },
 })
-
